0b1adb22f6c7724d584ff60f0b16fa7307ed3e34
zagamendes <zagamendes@users.noreply.github.com>
2022-12-03 16:02:42 -0300

    changed logout button

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App({ signOut }) {
         <Image src={logo} className="App-logo" alt="logo" />
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
-      <Button onClick={signOut}>Sign Out</Button>
+      <Button onClick={signOut}>Sair</Button>
     </View>
   );
 }
